436bab957c392d2b30ac13aab85bc11f28193a72
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2023-07-26 18:25:36 -0300

    Tip Calculator App

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
 const inputs = document.querySelectorAll("input");
+const resetButton = document.querySelector(".resetButton");
+const tipButton = document.querySelectorAll(".tipButton");
+let verifyInputs = () => {
+    for (let i = 0; i < inputs.length; i++) {
+        !inputs[i].value ? resetButton.classList.add("disabled") : resetButton.classList.remove("disabled");
+    }
+}
+
 for (let i = 0; i < inputs.length; i++) {
+    verifyInputs();
     inputs[i].addEventListener("input", () => {
+        verifyInputs();
         inputs[i].value = inputs[i].value.replace(/[a-z]/ig, "");
+    });
+}
+for (let i = 0; i < tipButton.length; i++) {
+    tipButton[i].addEventListener("click", function() {
+        for (let j = 0; j < tipButton.length; j++) {
+            this.style.background = "var(--color1)";    
+        }
+        this.style.background = "var(--color1)";
+        this.style.color = "var(--color2)";
     });
 }
