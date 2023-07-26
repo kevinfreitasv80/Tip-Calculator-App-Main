a883169ce314d3fe709d91dcb379fdb66aeb80d9
kevinfreitasv80 <kevinfreitasv80@gmail.com>
2023-07-26 16:43:19 -0300

    Tip Calculator App

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-let inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("input", () => {
-        inputs[i].value = ("/[a-z]/ig", "");
+        inputs[i].value = inputs[i].value.replace(/[a-z]/ig, "");
     });
 }
